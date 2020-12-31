5cd1160ef1ccc840af29410b8d8444f0f7b30973
saurabhnegi1 <58848146+saurabhnegi1@users.noreply.github.com>
2021-01-01 00:46:35 +0530

    Exercise 1.4

--- CppPrimer/Chapter1/6.cpp
+++ CppPrimer/Chapter1/6.cpp
@@ -4,7 +4,7 @@ int main(){
     std::cout << "Enter two number : ";
     int v1 = 0, v2 = 0; 
     std::cin >> v1 >> v2;
-
+    // only work if v2 is greater than v1 
     while(v1<=v2){
         std::cout << v1 << std::endl;
         ++v1;
