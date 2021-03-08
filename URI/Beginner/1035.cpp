acb6b7989a89d6e2ee26a75d0c0569f4ce2daf8c
Saurabh Negi <saurabhnegi9990@gmail.com>
2021-03-08 19:56:44 +0530

    Selection Test 1

---
+++ URI/Beginner/1035.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+
+    if(b>c && d>a && (c+d)>(a+b)){
+        if((c>0) && (d>0))    
+            if(a%2==0)
+                cout << "Valores aceitos" << "\n";
+    }else 
+        cout << "Valores nao aceitos" << "\n";
+}
